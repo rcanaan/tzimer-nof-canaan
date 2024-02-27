8b799303cd131ea21e0ee5a04139635fdfc97c3b
Rinat Canaan <58044154+rcanaan@users.noreply.github.com>
2024-02-27 23:07:30 +0200

    booking com links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "./Footer.module.css";
 import Image from "next/image";
 
@@ -5,12 +6,14 @@ export default function Footer() {
   return (
     <div className={styles.footer}>
       <div className={styles["footer__google-maps"]}>
-        <Image
-          width={400}
-          height={200}
-          src="/map.PNG"
-          alt="Google Maps location"
-        />
+        <Link href="https://www.booking.com/hotel/il/x-xxps-xx-xc-x-nof-canaan.he.html?aid=304142&label=gen173nr-1FCAQoggI49ANIDlgEaGqIAQGYAQ64ARfIAQzYAQHoAQH4AQKIAgGoAgO4Aq2a-a4GwAIB0gIkMzJjZWY0ZTctZWRiZi00MWUyLWEwMDgtZDY3OGU2NzBmZmZm2AIF4AIB&sid=c6f9a76c51f8fc0fc3e06022cc2384dd&checkin=2024-02-27;checkout=2024-02-28;dist=0;group_adults=2;group_children=0;hapos=1;hpos=1;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;soh=1;sr_order=popularity;srepoch=1709067635;srpvid=d60893af1c77048f;type=total;ucfs=1&#map_opened-hotel_header">
+          <Image
+            width={400}
+            height={200}
+            src="/map.PNG"
+            alt="Google Maps location"
+          />
+        </Link>
       </div>
 
       <div className={styles["footer__contact-info"]}>
--- components/Header.tsx
+++ components/Header.tsx
@@ -24,9 +24,6 @@ export default function Header() {
 
   return (
     <>
-      {/* <head>
-        <link rel="shortcut icon" href="/nofCanaanIcon.ico" />
-      </head> */}
       <header
         className={styles.header}
         style={{ backgroundImage: `url(${imagePaths[currentImageIndex]})` }}
@@ -59,7 +56,7 @@ export default function Header() {
               />
             </Link>
           </div>
-          <Link href="https://www.booking.com/searchresults.he.html">
+          <Link href="https://www.booking.com/hotel/il/x-xxps-xx-xc-x-nof-canaan.he.html?aid=304142&label=gen173nr-1FCAQoggI49ANIDlgEaGqIAQGYAQ64ARfIAQzYAQHoAQH4AQKIAgGoAgO4Aq2a-a4GwAIB0gIkMzJjZWY0ZTctZWRiZi00MWUyLWEwMDgtZDY3OGU2NzBmZmZm2AIF4AIB&sid=c6f9a76c51f8fc0fc3e06022cc2384dd&checkin=2024-02-27;checkout=2024-02-28;dist=0;group_adults=2;group_children=0;hapos=1;hpos=1;no_rooms=1;req_adults=2;req_children=0;room1=A%2CA;sb_price_type=total;soh=1;sr_order=popularity;srepoch=1709067635;srpvid=d60893af1c77048f;type=total;ucfs=1&#no_availability_msg">
             <button className={styles["header__menu-button"]}>
               RESERVE NOW
             </button>
