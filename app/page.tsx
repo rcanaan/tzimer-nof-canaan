72a130d412c0904df1cf9aa05cf260bbeb1ddf3d
Rinat Canaan <58044154+rcanaan@users.noreply.github.com>
2024-02-27 15:38:37 +0200

    remove unused code and components

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,15 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
-import Room from "@/components/room";
-import Link from "next/link";
-
 import Outdoor from "@/components/OutdoorCards";
-
 import { rooms } from "@/db/rooms";
 import { outdoors } from "@/db/outdoors";
 import RoomCards from "@/components/RoomCards";
 import Video from "@/components/Video";
 
 export default function Home() {
-  rooms;
   return (
     <>
-      <div className={styles.all}>
-        <RoomCards rooms={rooms} />
-        <Outdoor outdoors={outdoors} />
-        <Video />
-      </div>
+      <RoomCards rooms={rooms} />
+      <Outdoor outdoors={outdoors} />
+      <Video />
     </>
   );
 }
