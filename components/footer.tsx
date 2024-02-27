d3fcf33961573eb056fe8ce7f7f37b0414afdba9
Rinat Canaan <58044154+rcanaan@users.noreply.github.com>
2024-02-27 16:09:53 +0200

    update small  leters

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,4 @@
-import styles from "./Footer.module.css";
+import styles from "./footer.module.css";
 import Image from "next/image";
 
 export default function Footer() {
--- components/video.tsx
+++ components/video.tsx
@@ -1,4 +1,4 @@
-import styles from "./Video.module.css";
+import styles from "./video.module.css";
 
 export default function Video() {
   return (
