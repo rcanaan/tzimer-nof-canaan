c3ea44bbf3767a317cca5e9dffb55ce95f2f2812
Rinat Canaan <58044154+rcanaan@users.noreply.github.com>
2024-04-17 19:31:49 +0300

    erasing redundant

--- app/@modal/(.)rooms/[id]/page.tsx
+++ app/@modal/(.)rooms/[id]/page.tsx
@@ -3,17 +3,6 @@
 import { Modal } from "./modal";
 import { RoomId } from "@/db/rooms";
 
-// export default function Page({ params }: { params: { id: string } }) {
-//   const [isModalOpen, setIsModalOpen] = useState(true); // Assuming you want the modal to be open by default
-
-//   const handleCloseModal = () => {
-//     setIsModalOpen(false);
-//   };
-//   console.log(`Is modal open: ${isModalOpen}`);
-//   <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-//     {params.id}
-//   </Modal>;
-// }
 export default function Page({ params: { id } }: { params: { id: RoomId } }) {
   return <Modal>{id}</Modal>;
 }
