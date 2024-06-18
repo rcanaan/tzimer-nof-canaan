2f2975d6acc5df78a7636c18983239e8e1d667b9
Rinat Canaan <58044154+rcanaan@users.noreply.github.com>
2024-06-18 14:27:34 +0300

    feat - update screen by css and not by hook

--- components/RoomCards/RoomCards.tsx
+++ components/RoomCards/RoomCards.tsx
@@ -3,34 +3,30 @@ import styles from "./RoomCards.module.css";
 import { rooms } from "../../db/rooms";
 import { outdoors } from "../../db/outdoors";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import useScreenSize from "@/hooks/useScreenSize";
 import CardsMobile from "./CardsMobile";
 import CardsDesktop from "./CardsDesktop";
 
 export default function RoomCards() {
-  const { width } = useScreenSize();
   return (
     <>
-      {width > 1023 ? (
-        <>
-          <h2 className={styles["room-cards__main-heading"]}>The Rooms</h2>
-          <CardsDesktop cards={rooms} />
-          <h2 className={styles["room-cards__main-heading"]}>
-            Outdoor Activities
-          </h2>
+      <div className={styles.desktop}>
+        <h2 className={styles["room-cards__main-heading"]}>The Rooms</h2>
+        <CardsDesktop cards={rooms} />
+        <h2 className={styles["room-cards__main-heading"]}>
+          Outdoor Activities
+        </h2>
 
-          <CardsDesktop cards={outdoors} />
-        </>
-      ) : (
-        <>
-          <h3 className={styles["room-cards__main-heading"]}>The Rooms</h3>
-          <CardsMobile cards={rooms} />
-          <h3 className={styles["room-cards__main-heading"]}>
-            Outdoor Activities
-          </h3>
-          <CardsMobile cards={outdoors} />
-        </>
-      )}
+        <CardsDesktop cards={outdoors} />
+      </div>
+
+      <div className={styles.mobile}>
+        <h3 className={styles["room-cards__main-heading"]}>The Rooms</h3>
+        <CardsMobile cards={rooms} />
+        <h3 className={styles["room-cards__main-heading"]}>
+          Outdoor Activities
+        </h3>
+        <CardsMobile cards={outdoors} />
+      </div>
     </>
   );
 }
