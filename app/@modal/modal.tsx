faa86e9f7b64f6ca2bbf8a292a34b639252739d2
Rinat Canaan <58044154+rcanaan@users.noreply.github.com>
2024-06-29 22:23:53 +0300

    fixing modal data

--- app/@modal/modal.tsx
+++ app/@modal/modal.tsx
@@ -14,6 +14,13 @@ type ModalProps = {
   data: Rooms | Outdoors;
   isRoom?: boolean;
 };
+
+function isRoomId(id: RoomId | OutdoorId): id is RoomId {
+  return rooms.hasOwnProperty(id);
+}
+function isOutdoorId(id: RoomId | OutdoorId): id is OutdoorId {
+  return !isRoomId(id);
+}
 export function Modal({ id, data, isRoom }: ModalProps) {
   const router = useRouter();
   const dialogRef = useRef<ElementRef<"dialog">>(null);
@@ -28,19 +35,35 @@ export function Modal({ id, data, isRoom }: ModalProps) {
     router.back();
   }
 
+  let imageSrc = "";
+  let pageTitle = "";
+  let description = "";
+
+  if (isRoomId(id)) {
+    const roomData = data as Rooms;
+    imageSrc = roomData[id].imageSrc;
+    pageTitle = roomData[id].pageTitle;
+    description = roomData[id].description;
+  } else {
+    const outdoorData = data as Outdoors;
+    imageSrc = outdoorData[id].imageSrc;
+    pageTitle = outdoorData[id].pageTitle;
+    description = outdoorData[id].description;
+  }
+
   return createPortal(
     <div className={styles.modal_backdrop}>
       <dialog ref={dialogRef} className={styles.modal} onClose={onDismiss}>
         <div className={styles.modal_content}>
           <Image
-            src={data[id].imageSrc}
+            src={imageSrc}
             height={300}
             width={350}
-            alt={`${data[id].pageTitle}  `}
+            alt={`${pageTitle}`}
             className={styles.room__image}
           />
-          <h2 className={styles.room__title}>{data[id].pageTitle}</h2>
-          <p className={styles.room__description}>{data[id].description}</p>
+          <h2 className={styles.room__title}>{pageTitle}</h2>
+          <p className={styles.room__description}>{description}</p>
         </div>
         <button onClick={onDismiss} className={styles.close_button} />
       </dialog>
